125864a6aed3f4acb114d44d00c8b93a75f3653a
Kay <kahachang@gmail.com>
2020-07-27 14:08:28 +0800

    fix: search functin

--- app.js
+++ app.js
@@ -93,16 +93,20 @@ app.post('/restaurants/:id/delete', (req, res) => {
 // search
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter((restaurant) => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-  })
-  res.render('index', { restaurant: restaurants, keyword: keyword })
+  return Restaurant.find({ name: { $regex: keyword, $options: "i" } })
+    .lean()
+    .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.log(error))
+  // const restaurants = restaurantList.results.filter((restaurant) => {
+  //   return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  // // })
+  // res.render('index', { restaurant: restaurants, keyword: keyword })
 })
 
-app.get('/restaurants/:id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant })
-})
+// app.get('/restaurants/:id', (req, res) => {
+//   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+//   res.render('show', { restaurant })
+// })
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
