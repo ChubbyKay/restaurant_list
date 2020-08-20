f5b1150bb5482cee1a68b18b8db66d7f7e63fb92
Kay <kahachang@gmail.com>
2020-08-20 11:58:56 +0800

    feat: add login page & register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,4 +6,14 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+// login button
+router.post('/login', (req, res) => {
+
+})
+
+// register page
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 module.exports = router
